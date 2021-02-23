0794938a1887b95191e1e4a7a931f9b409c81d35
sbrevolution5 <Sbrevolution5@aim.com>
2021-02-23 11:48:35 -0500

    added color methods

--- gameui.py
+++ gameui.py
@@ -31,14 +31,49 @@ class Spot:
     def get_pos(self):
         return (self.col, self.row)
     #color change methods 
+    def make_wall(self):
+        self.color = BLACK;
+    def make_start(self):
+        self.color = ORANGE
+    def make_end(self):
+        self.color = TURQUOISE
+    def make_closed(self):
+        self.color = RED
+    def make_open(self):
+        self.color = GREEN
+    def reset(self):
+        self.color = WHITE
+    def make_path(self):
+        self.color = PURPLE
     # Boolean accessors is start/is end etc
+    def is_wall(self):
+        return self.color == BLACK
+    def is_start(self):
+        return self.color == ORANGE
+    def is_end(self):
+        return self.color == TURQUOISE
+    def is_closed(self):
+        return self.color == RED
+    def is_open(self):
+        return self.color == GREEN
     # function to fill in neighbors
-
+    def update_neighbors(self,grid):
+        
 
 
 # create an array (grid) of Spots,
+def make_grid(rows, width):
+    grid = []
+    for i in range(0,width):
+        grid.append([])
+        for j in range(0,height):
+            spot = Spot(i,j,width,height)
+            grid[i].append(spot)
+    return grid
+        
 # Draw a grid on screen
 def draw_grid(win, rows, width):
+
 # when user clicks mouse on grid, they change the color of the cell
 # Allow for a few small buttons to place start and end
 # draws background 
