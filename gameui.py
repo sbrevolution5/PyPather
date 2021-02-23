edd40362d8aa83cc0b97a07cb469c7d2f460eb0d
sbrevolution5 <Sbrevolution5@aim.com>
2021-02-23 11:34:29 -0500

    put prototypes for functions

--- gameui.py
+++ gameui.py
@@ -6,13 +6,16 @@ WIDTH = 800
 WINDOW = pygame.display.set_mode((WIDTH,WIDTH))#always square
 pygame.display.set_caption("Path finding")
 #Color constants-------
-WHITE = (255,255,255)
-BLACK = (0,0,0)
 RED = (255, 0, 0)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
-ORANGE = (255,155,0)
-TEAL = (64,244,208)
+GREEN = (0, 255, 0)
+BLUE = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165 ,0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
 #Classes --------
 class Spot:
     def __init__(self, row, col, width, height):
@@ -27,8 +30,21 @@ class Spot:
     # determine position
     def get_pos(self):
         return (self.col, self.row)
+    #color change methods 
+    # Boolean accessors is start/is end etc
+    # function to fill in neighbors
 
-    
+
+
+# create an array (grid) of Spots,
 # Draw a grid on screen
+def draw_grid(win, rows, width):
 # when user clicks mouse on grid, they change the color of the cell
 # Allow for a few small buttons to place start and end
+# draws background 
+def draw(win, grid, rows, width):
+
+# where did the user click?
+def get_clicked_pos(pos, rows, width):
+#game loop
+def main():
